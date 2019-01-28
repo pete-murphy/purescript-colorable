66cfb36a73b3b40ac28da116876bf400ca3b04e4
Peter Murphy <peter.francis.murphy@gmail.com>
2019-01-27 21:02:26 -0500

    Fixes bug: I was not using the useHooks API correctly
    When I updated state, using setColor as if it were the traditional setState API, I was actually erasing the existing state; whichever color I wasn't editing was being set to undefined

--- src/App.js
+++ src/App.js
@@ -3,29 +3,23 @@ import "./App.css"
 import { getLuminance, getContrast } from "./output/Colorable"
 
 const App = () => {
-  let [colorA, setColorA] = useState("#fff")
-  let [colorB, setColorB] = useState("#0ff")
-  const [a, b] = [colorA, colorB]
+  let [{ a, b }, setColor] = useState({ a: "#fff", b: "#ff0" })
   const handleChange = key => ({ target: { value } }) => {
-    switch (key) {
-      case "a":
-        setColorA(value)
-      case "b":
-        setColorB(value)
-      default:
-        break
-    }
-    console.log(getContrast(a)(b))
+    setColor(colors => ({ ...colors, [key]: value }))
   }
   const handleClick = () => {
     document.documentElement.style.setProperty("--background-color", a)
   }
+
   return (
-    <main>
-      <input value={a} onChange={handleChange("a")} type="text" />
-      <input value={b} onChange={handleChange("b")} type="text" />
-      <button onClick={handleClick}>Submit</button>
-    </main>
+    <>
+      <header>{getContrast(a)(b)}</header>
+      <main>
+        <input value={a} onChange={handleChange("a")} type="text" />
+        <input value={b} onChange={handleChange("b")} type="text" />
+        <button onClick={handleClick}>Submit</button>
+      </main>
+    </>
   )
 }
 
